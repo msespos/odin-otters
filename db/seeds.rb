07069f72c1fe3291843be4c7420eb8c457c89115
Mike Esposito <mikeesposito6852@gmail.com>
2021-11-30 08:39:56 -0800

    Add otters to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Otter.create([{ name: 'Mr. Otter', age: 5, cuteness: 10, softness: 10 },
+              { name: 'Mr. River Otter', age: 3, cuteness: 10, softness: 10 },
+              { name: 'Rando Q. Ottersby', age: 8, cuteness: 8, softness: 6 },
+              { name: 'Xavier P. Ottersbottom', age: 12, cuteness: 7, softness: 7 },
+              { name: 'Dan', age: 5, cuteness: 5, softness: 5 }])
