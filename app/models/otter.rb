9eba85e344e8b0c98db62c481c0d68767052925d
Mike Esposito <mikeesposito6852@gmail.com>
2021-12-01 07:14:08 -0800

    Add validations to Otter

--- app/models/otter.rb
+++ app/models/otter.rb
@@ -1,2 +1,8 @@
 class Otter < ApplicationRecord
+  validates :name, presence: true
+  validates :age, numericality: { only_integer: true }
+  validates :cuteness, numericality: { only_integer: true, greater_than: 0,
+                                       less_than_or_equal_to: 10 }
+  validates :softness, numericality: { only_integer: true, greater_than: 0,
+                                       less_than_or_equal_to: 10 }
 end
